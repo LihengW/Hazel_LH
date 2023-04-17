5c4ed9b9b4dd07a37f6ca936cd706cb2ecd7f38d
yukio <525822925@qq.com>
2023-04-17 16:43:24 +0800

    Event-System & precompiled headerfiles

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // For use by Hazel APP
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ------Entry point-------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,4 +1,7 @@
+#include "hzpch.h"
 #include "Application.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
 
 Hazel::Application::Application()
 {
@@ -10,5 +13,15 @@ Hazel::Application::~Application()
 
 void Hazel::Application::Run()
 {
+	WindowResizeEvent e(1280, 720);
+	if (e.IsInCategory(EventCategoryApplication))
+	{
+		HZ_TRACE(e);
+	}
+	if (e.IsInCategory(EventCategoryInput))
+	{
+		HZ_TRACE(e);
+	}
+
 	while (true);
 }
